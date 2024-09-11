c55b0c459e320cfaa138ba3da91254a281c7af01
Fazel Momeni <wwwfazel88@gmail.com>
2024-09-11 23:16:52 +0330

    add some fields

--- src/dj_backup/forms.py
+++ src/dj_backup/forms.py
@@ -6,26 +6,26 @@ from . import models
 class DJFileBackUpForm(forms.ModelForm):
     class Meta:
         model = models.DJFileBackUp
-        exclude = ('count_run',)
+        exclude = ('count_run', 'has_running_task')
 
 
 class DJFileBackUpUpdateForm(forms.ModelForm):
     class Meta:
         model = models.DJFileBackUp
-        exclude = ('count_run', 'files', 'schedule_task', 'results')
+        exclude = ('count_run', 'files', 'schedule_task', 'results', 'has_running_task')
 
 
 class DJDataBaseBackUpForm(forms.ModelForm):
     class Meta:
         model = models.DJDataBaseBackUp
-        exclude = ('count_run',)
+        exclude = ('count_run', 'has_running_task')
 
 
 class DJDataBaseBackUpUpdateForm(forms.ModelForm):
     class Meta:
         model = models.DJDataBaseBackUp
         exclude = ('count_run', 'files', 'schedule_task', 'results',
-                   'database', 'database_type', 'additional_args')
+                   'database', 'database_type', 'additional_args', 'has_running_task')
 
 
 class DJFileForm(forms.ModelForm):
