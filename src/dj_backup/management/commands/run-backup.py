d76c04df116ba47ddae2c91f4af1a5c836d5f13f
Fazel Momeni <fzl8747@gmail.com>
2025-06-07 11:58:39 +0330

    initial tasks(job) when run-backup

--- src/dj_backup/management/commands/run-backup.py
+++ src/dj_backup/management/commands/run-backup.py
@@ -1,7 +1,10 @@
 from django.core.management.base import BaseCommand
 
 from dj_backup import settings
-from dj_backup.core import utils, storages
+from dj_backup.core import utils, storages, tasks
+from dj_backup.models import DJFileBackUp, DJDataBaseBackUp, TaskSchedule
+
+from dj_backup.core.task import runner
 
 
 class Command(BaseCommand):
@@ -18,15 +21,33 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('INITIAL STORAGES...'))
         self.initial_storages()
 
+        self.stdout.write('INITIAL BACKUP TASKS...')
+        self.initial_tasks_job()
+        # start task handler
         self.stdout.write(self.style.SUCCESS('TASK RUNNER STARTED ..!'))
-
-        from dj_backup.core.task import runner
         runner.run()
 
     @staticmethod
     def initial_storages():
         # create storages object
         storages.initial_storages_obj()
+
+    @staticmethod
+    def initial_tasks_job():
+        # get backup tasks job
+        file_backups = list(DJFileBackUp.objects.filter(schedule_task__isnull=False))
+        db_backups = list(DJDataBaseBackUp.objects.filter(schedule_task__isnull=False))
+
+        # delete old tasks(job)
+        TaskSchedule.objects.all().delete()
+        runner.BackgroundScheduler.flush()
+        runner.BackgroundScheduler.flush_delete_tasks()
+        # initial backup tasks job
+        for fb in file_backups:
+            tasks.ScheduleFileBackupTask(fb)
+
+        for dbb in db_backups:
+            tasks.ScheduleDataBaseBackupTask(dbb)
 
     @staticmethod
     def create_backup_dirs():
