e537e80d095ceec30b4b93fe5bfe6d9b99b688b8
Fazel Momeni <fzl8747@gmail.com>
2025-06-07 12:41:42 +0330

    update telegram storage upload structure - some modift in styles

--- src/dj_backup/core/logging.py
+++ src/dj_backup/core/logging.py
@@ -14,18 +14,21 @@ def log_event(msg, level='info', exc_info=False, **kwargs):
     level = level.upper()
     level_n = LOG_LEVELS_NUM[level]
     logger.log(level_n, msg=msg, exc_info=exc_info, **kwargs)
-    exc = None
+    exc = ''
     if exc_info:
         # call triggers
         exception_type, exception_value, trace = sys.exc_info()
         tr = traceback.extract_tb(trace, 1)
         if tr:
             tr = tr[0]
-        exc = f"""
-            type: `{exception_type}`\n
-            value: `{exception_value}`\n
-            file: `{tr.filename}`\n
-            line: `{tr.lineno}`\n
-            exc_line: `{tr.line}`
-        """
+        try:
+            exc = f"""
+                type: `{exception_type}`\n
+                value: `{exception_value}`\n
+                file: `{tr.filename}`\n
+                line: `{tr.lineno}`\n
+                exc_line: `{tr.line}`
+            """
+        except Exception:
+            pass
     TriggerLogBase.call_trigger(level, level_n, msg, exc, [], **kwargs)
