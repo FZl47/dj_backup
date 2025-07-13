6a38e09cb3952ceffab00dc7de83186d97dd7c9e
Fazel Momeni <fzl8747@gmail.com>
2025-07-13 05:30:15 +0330

    update dropbox authorization flow(oauth)

--- src/dj_backup/core/storages/drop_box.py
+++ src/dj_backup/core/storages/drop_box.py
@@ -1,4 +1,5 @@
 import warnings
+import getpass
 
 try:
     import dropbox
@@ -15,11 +16,13 @@ from dj_backup.core import utils
 
 from .base import BaseStorageConnector
 
+_oauth_refresh_token = None
+
 
 class DropBoxConnector(BaseStorageConnector):
     IMPORT_STATUS = package_imported
     CONFIG = {
-        'ACCESS_TOKEN': None,
+        'APP_KEY': None,
         'OUT': '/dj_backup/'
     }
     STORAGE_NAME = 'DROPBOX'
@@ -31,7 +34,7 @@ class DropBoxConnector(BaseStorageConnector):
             create connection to host server
         """
         c = cls.CONFIG
-        dbx = dropbox.Dropbox(c['ACCESS_TOKEN'])
+        dbx = dropbox.Dropbox(oauth2_refresh_token=_oauth_refresh_token, app_key=c['APP_KEY'])
         cls.DBX = dbx
         return dbx
 
@@ -53,3 +56,41 @@ class DropBoxConnector(BaseStorageConnector):
         output = utils.join_paths(self.CONFIG['OUT'], file_name)
         self.upload(dbx, output)
         self.close()
+
+    @classmethod
+    def check_before_setup(cls):
+        if cls.IMPORT_STATUS is False:
+            return False
+        return True
+
+    @classmethod
+    def setup(cls):
+        if not cls.check_before_setup():
+            return
+
+        global _oauth_refresh_token
+        c = cls.CONFIG
+        auth_flow = dropbox.DropboxOAuth2FlowNoRedirect(c['APP_KEY'], use_pkce=True, token_access_type='offline')
+
+        authorize_url = auth_flow.start()
+
+        W_BOLD = "\033[1;37m"
+        BLUE_BOLD = "\033[1;34m"
+        G_BOLD = "\033[1;32m"
+        RESET = "\033[0m"
+
+        print(f"{BLUE_BOLD}--- Setup DropBox Access(OAUTH) Started ! ---{RESET}")
+        print(f"{W_BOLD}1. Go to: {authorize_url}{RESET}")
+        print(f"{W_BOLD}2. Click \"Allow\" (you might have to log in first).{RESET}")
+        print(f"{W_BOLD}3. Copy the authorization code.{RESET}")
+
+        try:
+            auth_code = getpass.getpass(f"{W_BOLD}Enter the authorization code here: {RESET}").strip()
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt('Setup dropbox access failed [You must enter authorization code]')
+
+        oauth_result = auth_flow.finish(auth_code)
+
+        _oauth_refresh_token = oauth_result.refresh_token
+
+        print(f"{G_BOLD}--- Setup DropBox Access(OAUTH) Finished Successfully ! ---{RESET}")
